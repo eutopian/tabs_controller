61df39a18bdda50806d4194df6ee93ac01610dd2
Margaret Ma <eutopia@gmail.com>
2018-03-29 15:57:33 -0400

    First commit

---
+++ background.js
@@ -0,0 +1,9 @@
+chrome.tabs.onCreated.addListener(function(tab) {
+  chrome.tabs.getAllInWindow(null, function(tabs){
+    if (tabs.length > 5) {
+      alert("YOU HAVE TOO MANY TABS OPEN!")
+      chrome.tabs.remove(tabs[0].id);
+      window.location.reload();
+    }
+  });
+});
